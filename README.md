7a89dc15f7b7aadd784d7dac026ac3999ae39b1e
bcol120 <bryant.cole@nerdintel.com>
2022-04-04 20:36:53 -0400

    remove updater and small bug fixes

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 Short description. No more than 150 chars.
 
 == Description ==
-The IdeaKit main theme
+The ProjectKit main theme
 
 == Frequently Asked Questions ==
 
@@ -24,5 +24,3 @@ An answer to that question.
 * Initial Release
 
 == Resources ==
-* magnify.jpg © 2014 Jane Doe, CC0
-* supermenu.js © 2013-2015 James Today, MIT
--- functions.php
+++ functions.php
@@ -11,17 +11,3 @@ function projectkit_ct_enqueue_parent() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
 add_action( 'wp_enqueue_scripts', 'projectkit_ct_enqueue_parent' );
-
-//Check for theme updates
-require 'plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-  'https://github.com/Nerd-Intelligence-Agency/projectkit-theme',
-  __FILE__,
-  'ideakit-theme'
-);
-
-//Optional: If you're using a private repository, specify the access token like this:
-//$myUpdateChecker->setAuthentication('your-token-here');
-
-//Optional: Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('master');
